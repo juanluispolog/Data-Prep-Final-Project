9d88a644e6b3eb64d7c75b440b9d3b0319ead3ee
Juan Luis Polo <jlpolog  git config --global user.name Your>
2019-11-13 13:08:47 -0600

    added dataset from season 19/20. Not implemented as time series

--- classificationModels.R
+++ classificationModels.R
@@ -10,11 +10,12 @@ df.15.16 <- prepDataset(read.csv("premier-15-16.csv"))
 df.16.17 <- prepDataset(read.csv("premier-16-17.csv"))
 df.17.18 <- prepDataset(read.csv("premier-17-18.csv"))
 df.18.19 <- prepDataset(read.csv("premier-18-19.csv"))
+df.19.20 <- prepDataset(read.csv("premier-19-20.csv"))
 
 # Merging datasets:
-df <- rbind(df.15.16, df.16.17, df.17.18, df.18.19)
+df <- rbind(df.15.16, df.16.17, df.17.18, df.18.19, df.19.20)
 
-# Creating data frame for given team:
+# Creating data frames for given team:
 df <- wld(df, "Chelsea")
 
 # Create data partition: test/train:
@@ -33,12 +34,18 @@ library(rpart)
 library(rpart.plot)
 
 
-# DT with all attributes:
-dt.model <- rpart(WLD ~ Season + SF + HomeTeam + AwayTeam + Referee, method='class', data=df.train)
+# DT with attributes Season, SF, HomeTeam, AwayTeam, Referee:
+df.train <- selectPred(df.train, "WLD")
+df.test <- selectPred(df.test, "WLD")
+
+dt.model <- rpart(WLD ~ ., method='class', data=df.train)
 rpart.plot(dt.model, extra = 104, type = 4, fallen.leaves = T, 
            main = "Decision Tree on WLD")
 dt.pred<- predict(dt.model, df.test, type = "class")
 confusionMatrix(dt.pred, df.test$WLD)
+
+
+
 
 
 ### RANDOM FOREST:
@@ -46,7 +53,11 @@ confusionMatrix(dt.pred, df.test$WLD)
 # Using randomForest library for create the random forest models:
 library(randomForest)
 
-# RF with all attributes:
-rf.model <- randomForest(WLD ~ Season + SF + HomeTeam + AwayTeam + Referee, data = df.train)
+# RF with attributes Season, SF, HomeTeam, AwayTeam, Referee:
+df.train <- selectPred(df.train, "WLD")
+df.test <- selectPred(df.test, "WLD")
+
+set.seed(1122)
+rf.model <- randomForest(WLD ~ ., data = df.train, mtry = 5, ntree = 100)
 rf.pred <- predict(rf.model, df.test, type="class")
 confusionMatrix(rf.pred, df.test$WLD)
